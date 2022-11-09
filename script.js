0f620b43d4200cd2dad540e30c478314fb2cd11d
joaonunatingscode <joaonunatingscode@gmail.com>
2022-11-09 22:43:17 +0000

    Added start page and audio fix

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function terminalType() {
 	p.innerText = ""
 	p.style.opacity = '1'
 	let i = 0, speed = 50
+
 	function typeWriter() {
 		if (i < text.length) {
 			p.innerHTML += text.charAt(i)
@@ -12,5 +13,20 @@ function terminalType() {
 			setTimeout(typeWriter, speed)
 		}
 	}
+
 	typeWriter()
+}
+
+const audioPlayer = document.getElementById('audio-player')
+
+window.onload = () => {
+	window.addEventListener('click', async () => {
+		if (!audioPlayer.paused) return
+		const content = document.getElementById('content')
+		await transition.begin(content, OPACITY_ANIMATION.out).promise
+		changeContent('pages/home.html')
+		await Promise.all([transition.begin(content, OPACITY_ANIMATION.in).promise, showNavs([BOT_NAV, RIGHT_NAV, LEFT_NAV])])
+		audioPlayer.play()
+	})
+
 }
